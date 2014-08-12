4c55615580ea6641112ece282ef18c5942507b85
Kenji Yasumitsu <kenjiyasumitsu@gmail.com>
2014-08-12 00:24:56 -0300

    Starting project

---
+++ main/index.php
@@ -0,0 +1,13 @@
+<html>	
+<head>
+<title> GURPS Combat System Solver </title>
+</head>
+<body>
+<h1>GURPS Combat System Solver</h1>
+<table>
+    <tr>
+        <td id="armor">Armor</td>
+    </tr>
+</table>
+</body>
+</html>
